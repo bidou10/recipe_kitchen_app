315d99385b104b3b2f83493f56ff27a81ebe01a3
bidou10 <bidounomad@gmail.com>
2023-06-24 17:47:06 +1200

    login_page v1

---
+++ lib/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.amber,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
