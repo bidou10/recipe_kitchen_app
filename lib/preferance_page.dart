828f9e48e80b0bef6938c6a1e47ee3af27e594e8
bidou10 <bidounomad@gmail.com>
2023-06-26 16:26:01 +1200

    preferance_page v1

--- lib/preferance_page.dart
+++ lib/preferance_page.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_kitchen_app/first_page.dart';
 
 class PreferancePage extends StatelessWidget {
   const PreferancePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                            left: 30,
+                            right: 30,
+                          ),
+                          child: Text(
+                            "Do you follow any special diet ?",
+                            textAlign: TextAlign.justify,
+                            maxLines: 3,
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
