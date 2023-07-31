14f7eac9c3ceaf64ab200661273ba3b860be820d
bidou10 <bidounomad@gmail.com>
2023-07-31 13:22:44 +1200

    mediaQuery v1 first page

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -8,150 +8,157 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // double screenWidth = MediaQuery.of(context).size.width;  si je veux mettre
+    // double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Container(
-          width: double.infinity,
+          width:
+              MediaQuery.of(context).size.width, // utilise  80% de la largeur
           height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Welcome',
-                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Be the best cook with our recipes",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/icons/login.png'),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Welcome',
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Be the best cook with our recipes",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/login.png'),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Column(
-                children: <Widget>[
-                  MaterialButton(
-                    minWidth: 330,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ),
-                      );
-                    },
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                          color: Colors.black,
-                          style: BorderStyle.solid,
-                          width: 3),
-                      borderRadius: BorderRadius.circular(70),
-                    ),
-                    child: Text(
-                      'login',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 28,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    child: MaterialButton(
+                SizedBox(
+                  height: 70,
+                ),
+                Column(
+                  children: <Widget>[
+                    MaterialButton(
                       minWidth: 330,
                       height: 60,
                       onPressed: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => SignUpPage(),
+                            builder: (context) => LoginPage(),
                           ),
                         );
                       },
-                      color: Colors.amber,
                       shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                              color: Colors.black,
-                              style: BorderStyle.solid,
-                              width: 3),
-                          borderRadius: BorderRadius.circular(70)),
+                        side: BorderSide(
+                            color: Colors.black,
+                            style: BorderStyle.solid,
+                            width: 3),
+                        borderRadius: BorderRadius.circular(70),
+                      ),
                       child: Text(
-                        'Sign Up',
+                        'login',
                         style: TextStyle(
                           fontWeight: FontWeight.w700,
                           fontSize: 28,
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    child: const Text(
-                      'Don t have an account Sign Up',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          FontAwesomeIcons.facebook,
-                          color: Colors.blue,
-                          size: 23,
+                    Container(
+                      child: MaterialButton(
+                        minWidth: 330,
+                        height: 60,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignUpPage(),
+                            ),
+                          );
+                        },
+                        color: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Colors.black,
+                                style: BorderStyle.solid,
+                                width: 3),
+                            borderRadius: BorderRadius.circular(70)),
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 28,
+                          ),
                         ),
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          FontAwesomeIcons.google,
-                          color: Colors.redAccent,
-                          size: 23,
-                        ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      child: const Text(
+                        'Don t have an account Sign Up',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w600),
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          FontAwesomeIcons.linkedinIn,
-                          color: Colors.blueAccent,
-                          size: 23,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            FontAwesomeIcons.facebook,
+                            color: Colors.blue,
+                            size: 23,
+                          ),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              )
-            ],
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            FontAwesomeIcons.google,
+                            color: Colors.redAccent,
+                            size: 23,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            FontAwesomeIcons.linkedinIn,
+                            color: Colors.blueAccent,
+                            size: 23,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
