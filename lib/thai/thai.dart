c7ecc7d9967f92e8ef1ee2ec59419923cd6a8df4
bidou10 <bidounomad@gmail.com>
2023-08-25 17:31:19 +1200

    thaiMushroomOmelette loading

--- lib/thai/thai.dart
+++ lib/thai/thai.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_kitchen_app/thai/thaiClassic.dart';
+import 'package:recipe_kitchen_app/thai/thaiHolyBasil.dart';
+import 'package:recipe_kitchen_app/thai/thaiMushroomOmelette.dart';
 import 'package:recipe_kitchen_app/thai/thaiRedCurry.dart';
 import 'package:recipe_kitchen_app/thai/thaiShrimp.dart';
 import 'package:sticky_headers/sticky_headers.dart';
@@ -61,6 +63,21 @@ class _ThaiPageState extends State<ThaiPage> {
                     builder: (context) => ThaiRedCurry(),
                   ),
                 );
+              } else if (nameRecipe[index] ==
+                  "Thai Holy basil and pork stir fry") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ThaiHolyBasil(),
+                  ),
+                );
+              } else if (nameRecipe[index] == "Thai mushroom tuffed omelette") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ThaiMushroomOmelette(),
+                  ),
+                );
               }
             },
           ),
@@ -82,8 +99,8 @@ final nameRecipe = [
   "Pad Thai Shrimp",
   "Thai Stir Fried Noodles",
   "Thai Red Curry",
-  "Minced pork",
-  "Fried Chicken",
+  "Thai Holy basil and pork stir fry",
+  "Thai mushroom tuffed omelette",
   "Green Papaya",
   "Curry noodle",
   "Cut",
@@ -92,10 +109,10 @@ final nameRecipe = [
 
 final imageUrls = [
   "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/4/14/2/FNM060109WE031_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371589419461.jpeg",
-  "https://www.awesomecuisine.com/wp-content/uploads/2015/12/pad-see-ew.jpg",
+  "https://images.services.kitchenstories.io/OmFK5XJFzzF4iHAnUB1WYiUuORI=/1920x0/filters:quality(85)/images.kitchenstories.io/wagtailOriginalImages/R1569-photo-final-02.jpg",
   "https://images.services.kitchenstories.io/gqslNuqfstd1T9mtoE64Q1mTmgw=/1920x0/filters:quality(85)/images.kitchenstories.io/wagtailOriginalImages/R2960-final-photo-2.jpg",
-  "https://data.asiahighlights.com/image/travel-guide/thailand/thai-food/Minced-Pork-Stir-Fried-with-Thai-Basil.webp",
-  "https://data.asiahighlights.com/image/travel-guide/thailand/thai-food/Stir-Fried-Chicken-with-Cashew-Nuts.webp",
+  "https://images.services.kitchenstories.io/GaL2UgQSez4gVzBLRYdd-5h1pDo=/1920x0/filters:quality(85)/images.kitchenstories.io/wagtailOriginalImages/R2592-final-photo.jpg",
+  "https://images.services.kitchenstories.io/UAzPu-TxYoZOUKiSQi2aK6RsJE0=/1920x0/filters:quality(80)/images.kitchenstories.io/wagtailOriginalImages/R1881-photo-final-4.jpg",
   "https://data.asiahighlights.com/image/travel-guide/thailand/thai-food/Spicy-Green-Papaya-Salad.webp",
   "https://data.asiahighlights.com/image/travel-guide/thailand/thai-food/Thai-Coconut-Curry-Noodle-Soup.webp",
   "https://images.asiahighlights.com/allpicture/2022/02/ca9d3c345fb14950adf2d939_cut_750x400_264.webp",
